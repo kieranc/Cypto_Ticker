e01647ad5b846108b45d5ae4357421cac7747d16
kieranc <kieranc@gmail.com>
2017-12-18 11:39:50 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,4 +12,7 @@ xmrrate=0
 xmrchange=0
 offset=0
 counter=0
+-- Run getrates.lua every 60 seconds
 tmr.alarm(0, 60000, 1, function() dofile('getrates.lua') end )
+-- Run getrates.lua once, 8 seconds after boot.
+tmr.alarm(1, 8000, tmr.ALARM_SINGLE, function() dofile('getrates.lua') end )
